8260097d3ee1607667174af1c92d6ce1edc37e3e
mhmulford0 <mhmulford0@gmail.com>
2020-07-23 13:04:10 -0500

    finished task three, started on four

--- index.js
+++ index.js
@@ -84,7 +84,21 @@ class Person {
 */
 
 class Car {
+  constructor(model, milesPerGallon) {
+    this.model = model;
+    this.milesPerGallon = milesPerGallon;
+    this.tank = 0;
+    this.odometer = 0;
+  }
 
+  fill(gallons) {
+
+    this.tank += gallons;
+  }
+
+  drive(distance) {
+    this.odometer += distance;
+  }
 }
 
 /*
@@ -100,6 +114,16 @@ class Car {
         + {name} and {location} of course come from the instance's own properties.
 */
 class Lambdasian {
+
+  constructor(traits) {
+    this.name = traits.name;
+    this.age = traits.age;
+    this.location = traits.location;
+  }
+
+  speak() {
+    return `Hello my name is ${this.name}, I am from ${this.location}`
+  }
 
 }
 
@@ -117,7 +141,10 @@ class Lambdasian {
         + `demo` receives a `subject` string as an argument and returns the phrase 'Today we are learning about {subject}' where subject is the param passed in.
         + `grade` receives a `student` object and a `subject` string as arguments and returns '{student.name} receives a perfect score on {subject}'
 */
-class Instructor {
+class Instructor extends Lambdasian {
+  constructor(traits) {
+    super(traits)
+  }
 
 }
 
